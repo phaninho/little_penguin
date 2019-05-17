5e5e40ccb747f1d48eb01bb3ee708dfd06a13c66
phaninho <phaninho@live.fr>
2019-05-17 13:09:31 +0200

    add 09 done

--- 09/mounts.c
+++ 09/mounts.c
@@ -3,32 +3,76 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
+#include "../fs/mount.h"
+#include <linux/fs_struct.h>
 
-static struct proc_dir_entry *proc_entry;
+MODULE_AUTHOR("stmartin");
+MODULE_DESCRIPTION("proc mounted list module");
+MODULE_LICENSE("GPL");
 
-static ssize_t mywrite(struct file *file, const char *buffer,size_t len, loff_t *ppos)
+#define PROC_ENTRY_FILENAME "mymounts"
+#define BUFF_SIZE 256
+
+static struct proc_dir_entry	*proc_entry;
+static struct dentry		*curdentry;
+//static char			*message_begin_address;
+static char   			message[BUFF_SIZE] = {0};
+
+static void	append_message(void)
 {
-	printk(KERN_INFO "in mywrite\n");
-	return 1;
+	char	path_buff[256] = {0};
+	
+	printk(KERN_INFO "mounts: inside append_message [%s]\n", curdentry->d_name.name);
+	strcat(message, curdentry->d_name.name);
+	strcat(message, "\t");
+	strcat(message, dentry_path_raw(curdentry, path_buff, 256));
+	strcat(message, "\n");
 }
 
 static ssize_t myread(struct file *file, char *buffer,size_t len, loff_t *ppos) 
 {
-	printk(KERN_INFO "in myread\n");
-	return 1;
+	int	size_of_message;
+
+	if (message[0] == 0) {
+	/*	message_begin_address = message;
+		printk(KERN_INFO "on est laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
+	}
+	else
+	{
+		memset(message, 0, BUFF_SIZE);
+		message = message_begin_address;
+	}*/
+		printk(KERN_INFO "mounts: in myread\n");
+		list_for_each_entry(curdentry, &current->fs->root.mnt->mnt_root->d_subdirs, d_child)
+		{
+			printk(KERN_INFO "mounts: in list_for_each\n");
+			if (curdentry->d_flags & DCACHE_MOUNTED)
+				append_message();
+		}
+	}
+	size_of_message = strlen(message);
+	printk(KERN_INFO "size_of_message [%d]\n", size_of_message);
+	if (*ppos == 0 && len > 5) {
+		if (copy_to_user(buffer, message, size_of_message) != 0) {
+			printk(KERN_INFO "copy to user failed\n");
+			return -1;
+		}
+		*ppos = size_of_message;
+		return (size_of_message);
+	}
+	return 0;
 }
 
 static struct file_operations myops =
 {
 	.owner = THIS_MODULE,
 	.read = myread,
-	.write = mywrite,
 };
 
 static int 	__init mounts_init(void)
 {
 	printk(KERN_INFO "mounts: mounts module init!\n");
-	proc_entry = proc_create("mymounts", 0440, NULL, &myops);
+	proc_entry = proc_create(PROC_ENTRY_FILENAME, 0440, NULL, &myops);
 	if (proc_entry == NULL)
    		return -ENOMEM;
 	return 0;
